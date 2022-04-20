e44f05768d95d0ed7e19fea8fea0e64362c917ca
kemin <ted1994511@gmail.com>
2022-04-20 21:30:41 +0800

    更新utils

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+
+module.exports = defineConfig({
+  transpileDependencies: true
+})
 module.exports = {
   outputDir: 'docs',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Vuecalendar/'
-    : '/'
+    : '/',
 }
